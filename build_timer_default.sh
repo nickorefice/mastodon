83f325647c745c846573adf09cf828a0eeae9dba
norefice-github <nickjoref@gmail.com>
2024-12-04 11:27:04 -0600

    add build scripts

---
+++ build_timer_default.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+docker context use default
+start_time=$(date +%s)
+docker buildx build --tag demonstrationorg/mastodon:4.2.3 .
+end_time=$(date +%s)
+
+elapsed_time=$((end_time - start_time))
+
+printf "\nBuild completed in %02d:%02d:%02d (hh:mm:ss)\n" \
+  $((elapsed_time / 3600)) $(((elapsed_time % 3600) / 60)) $((elapsed_time % 60))
